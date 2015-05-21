75f1e392cc9d7676bed236d4b7783fd6d6cab7eb
Raycao50 <raycao50@gmail.com>
2015-05-22 03:02:15 +0800

    new version

--- G10316003_ex1/src/BestFirst.java
+++ G10316003_ex1/src/BestFirst.java
@@ -6,6 +6,8 @@ public class BestFirst {
 	public static void main(String args[]){
 		int[][] A = {{2,8,3},{1,6,4},{7,0,5}};
 		int[][] B = {{1,2,3},{8,0,4},{7,6,5}};
+		int Step = 1;
+		Scanner input = new Scanner(System.in);
 		
 		int count = 0 , finish = 0;
 		
@@ -16,16 +18,20 @@ public class BestFirst {
 		Display(B);
 		System.out.print("Here comes moving....\n");
 
-//		while(finish==0){
+		while(input.hasNext()){
+			int k = input.nextInt();
+			System.out.printf("Step :%d\n",Step);
 			int result = Compare(A,B);
 			System.out.printf("Current Wrong :%d\n",result);
 			if(result==0){
 				System.out.printf("We got it");
+				break;
 			}else{
-				Moving(A,B);
+				A=Moving(A,B);
+			    //result = Compare(A,B);
+				Step++;
 			}
-			
-//		}
+		}
 	}
 	
 	static void Display(int[][] a){
@@ -48,10 +54,11 @@ public class BestFirst {
 		return WP;
 	}
 	
-	static void Moving(int[][] a,int[][] b){
+	static int[][] Moving(int[][] a,int[][] b){
 		int index_i = 0 ,index_j = 0;
 		int nup,ndown,nleft,nright;
 		int up = 0 , down = 1 , left = 2, right = 3;
+		int count = 0;
 		int choose[] = new int[4]; 
 		
 		for(int i=0;i<3;i++){
@@ -68,19 +75,109 @@ public class BestFirst {
 		nleft = Compare(left(a,index_i,index_j),b);
 		nright = Compare(right(a,index_i,index_j),b);
 		
-		System.out.printf("Up :%d\n",nup);
-		System.out.printf("Down :%d\n",ndown);
-		System.out.printf("Left :%d\n",nleft);
+		System.out.printf("Up :%d  ",nup);
+		System.out.printf("Down :%d  ",ndown);
+		System.out.printf("Left :%d  ",nleft);
 		System.out.printf("Right :%d\n",nright);
 		
 		int min = Min(Compare(up(a,index_i,index_j),b),Compare(down(a,index_i,index_j),b),
 				Compare(left(a,index_i,index_j),b),Compare(right(a,index_i,index_j),b));
-		System.out.printf("min is :%d",min);
+		System.out.printf("min is :%d\n",min);
 		
-		if(min == nup){choose[up]++;}
-		if(min == ndown){choose[down]++;}
-		if(min == nleft){choose[left]++;}
-		if(min == nright){choose[right]++;}
+		if(min == nup){choose[up]++; count++;}
+		if(min == ndown){choose[down]++; count++;}
+		if(min == nleft){choose[left]++; count++;}
+		if(min == nright){choose[right]++; count++;}
+
+		
+		if(count>1){
+			int num = (int)(Math.random()*count) + 1;
+			System.out.printf("count is :%d\n",count);
+			System.out.printf("random is :%d\n",num);
+
+			for(int m=0;m<4;m++){
+				if(choose[m] == 1){
+					num--;
+					if(num == 0){
+						switch(m){
+						case 0:
+							System.out.print("Choose Up\n");
+							//Display(up(a,index_i,index_j));
+							a[index_i][index_j] = a[index_i - 1][index_j];
+							a[index_i - 1][index_j] = 0;
+							
+							break;
+							
+						case 1:
+							System.out.print("Choose Down\n");
+							//Display(down(a,index_i,index_j));
+							a[index_i][index_j] = a[index_i +1][index_j];
+							a[index_i + 1][index_j] = 0;
+							
+							break;
+							
+						case 2:
+							System.out.print("Choose Left\n");
+							//Display(left(a,index_i,index_j));
+							a[index_i][index_j] = a[index_i][index_j - 1];
+							a[index_i][index_j - 1] = 0;
+							
+							break;
+							
+						case 3:
+							System.out.print("Choose Right\n");
+							//Display(right(a,index_i,index_j));
+							a[index_i][index_j] = a[index_i][index_j + 1];
+							a[index_i][index_j + 1] = 0;
+							
+							break;
+						}
+						Display(a);
+					}
+				}
+			}
+		}else if(count == 1){
+			for(int i = 0;i<4;i++){
+				if(choose[i]==1){
+					switch(i){
+					case 0:
+						System.out.print("Choose Up\n");
+						//Display(up(a,index_i,index_j));
+						a[index_i][index_j] = a[index_i - 1][index_j];
+						a[index_i - 1][index_j] = 0;
+						
+						break;
+						
+					case 1:
+						System.out.print("Choose Down\n");
+						//Display(down(a,index_i,index_j));
+						a[index_i][index_j] = a[index_i +1][index_j];
+						a[index_i + 1][index_j] = 0;
+						
+						break;
+						
+					case 2:
+						System.out.print("Choose Left\n");
+						//Display(left(a,index_i,index_j));
+						a[index_i][index_j - 1] = a[index_i][index_j - 1];
+						a[index_i][index_j - 1] = 0;
+						
+						break;
+						
+					case 3:
+						System.out.print("Choose Right\n");
+						//Display(right(a,index_i,index_j));
+						a[index_i][index_j] = a[index_i][index_j + 1];
+						a[index_i][index_j + 1] = 0;
+						
+						break;
+					}
+					Display(a);
+				} 
+			}
+		}
+		
+		return a;
 		
 	}
 	
@@ -173,9 +270,14 @@ public class BestFirst {
 		return min;
 	}
 	
+	static int[][] newpuzzle(int[][] a){
+		return a;
+	}
+	
 	static int[][] before(int[][] a){
 		int[][] temp = a;
 		return temp;
 	}
+	
 	
 }
