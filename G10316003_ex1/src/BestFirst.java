e98e8589ed6814ab3f37971923b1f9d196bb9509
Raycao50 <raycao50@gmail.com>
2015-05-21 12:08:52 +0800

    Best first for the 8-puzzle problem

---
+++ G10316003_ex1/src/BestFirst.java
@@ -0,0 +1,181 @@
+//Using the Best First Algorithm to slove 8-puzzle problem
+
+import java.util.*;
+
+public class BestFirst {
+	public static void main(String args[]){
+		int[][] A = {{2,8,3},{1,6,4},{7,0,5}};
+		int[][] B = {{1,2,3},{8,0,4},{7,6,5}};
+		
+		int count = 0 , finish = 0;
+		
+		System.out.print("Origin:\n");
+		Display(A);
+		
+		System.out.print("Target:\n");
+		Display(B);
+		System.out.print("Here comes moving....\n");
+
+//		while(finish==0){
+			int result = Compare(A,B);
+			System.out.printf("Current Wrong :%d\n",result);
+			if(result==0){
+				System.out.printf("We got it");
+			}else{
+				Moving(A,B);
+			}
+			
+//		}
+	}
+	
+	static void Display(int[][] a){
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				System.out.printf("%d ",a[i][j]);
+			}
+				System.out.println("");
+		}					
+	}
+	
+	static int Compare(int[][] a,int[][] b){
+		int WP=0; //WP means the wrong position
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if(a[i][j]!=b[i][j])
+					WP++;
+			}
+		}
+		return WP;
+	}
+	
+	static void Moving(int[][] a,int[][] b){
+		int index_i = 0 ,index_j = 0;
+		int nup,ndown,nleft,nright;
+		int up = 0 , down = 1 , left = 2, right = 3;
+		int choose[] = new int[4]; 
+		
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if(a[i][j]==0){
+					index_i = i;
+					index_j = j;
+				}
+			}
+		}
+		
+		nup = Compare(up(a,index_i,index_j),b);
+		ndown = Compare(down(a,index_i,index_j),b);
+		nleft = Compare(left(a,index_i,index_j),b);
+		nright = Compare(right(a,index_i,index_j),b);
+		
+		System.out.printf("Up :%d\n",nup);
+		System.out.printf("Down :%d\n",ndown);
+		System.out.printf("Left :%d\n",nleft);
+		System.out.printf("Right :%d\n",nright);
+		
+		int min = Min(Compare(up(a,index_i,index_j),b),Compare(down(a,index_i,index_j),b),
+				Compare(left(a,index_i,index_j),b),Compare(right(a,index_i,index_j),b));
+		System.out.printf("min is :%d",min);
+		
+		if(min == nup){choose[up]++;}
+		if(min == ndown){choose[down]++;}
+		if(min == nleft){choose[left]++;}
+		if(min == nright){choose[right]++;}
+		
+	}
+	
+	static int[][] up(int[][] a,int index_i ,int index_j){
+		if(index_i == 0){
+			return a;
+		}else{
+		int[][] temp = new int[3][3];
+				
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				temp[i][j] = a[i][j];
+			}
+		}
+				
+		temp[index_i - 1][index_j] = 0;
+		temp[index_i][index_j] = a[index_i - 1][index_j];
+		
+		return temp;}
+	}
+	
+	static int[][] down(int[][] a,int index_i ,int index_j){
+		if(index_i == 2){
+			return a;
+		}else{
+		int[][] temp = new int[3][3];
+				
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				temp[i][j] = a[i][j];
+			}
+		}
+				
+		temp[index_i + 1][index_j] = 0;
+		temp[index_i][index_j] = a[index_i + 1][index_j];
+		
+		return temp;}
+	}
+	
+	static int[][] left(int[][] a,int index_i ,int index_j){
+		if(index_j == 0){
+			return a;
+		}else{
+		int[][] temp = new int[3][3];
+				
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				temp[i][j] = a[i][j];
+			}
+		}
+				
+		temp[index_i][index_j - 1] = 0;
+		temp[index_i][index_j] = a[index_i][index_j - 1];
+		
+		return temp;}
+	}
+	
+	static int[][] right(int[][] a,int index_i ,int index_j){
+		if( index_j == 2){
+			return a;
+		}else{
+		int[][] temp = new int[3][3];
+				
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				temp[i][j] = a[i][j];
+			}
+		}
+				
+		temp[index_i][index_j + 1] = 0;
+		temp[index_i][index_j] = a[index_i][index_j + 1];
+		
+		return temp;}
+	}
+	
+	static int Min(int a,int b,int c,int d){
+		int temp0,temp1,min;
+		if(a>b){
+			temp0 = b;
+		}else{ temp0 = a;}
+		
+		if(c>d){
+			temp1 = d;
+		}else{temp1 = c;}
+		
+		if(temp0>temp1){
+			min = temp1;
+		}else{min = temp0;}
+		
+		return min;
+	}
+	
+	static int[][] before(int[][] a){
+		int[][] temp = a;
+		return temp;
+	}
+	
+}
